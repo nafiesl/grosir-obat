bcb5a149977e79b6fa46ea3691c795ab5163a0fd
Nafies Luthfi <nafiesl@gmail.com>
2017-05-02 21:05:33 +0700

    Added unit lang file and fixed unit forms

---
+++ resources/lang/id/unit.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    // Labels
+    'unit'           => 'Satuan',
+    'list'           => 'Daftar Satuan',
+    'empty'          => 'Belum ada Satuan',
+    'back_to_index'  => 'Kembali ke daftar Satuan',
+
+    // Actions
+    'create'         => 'Input Satuan Baru',
+    'created'        => 'Input Satuan baru telah berhasil.',
+    'edit'           => 'Edit Satuan',
+    'update'         => 'Update Satuan',
+    'updated'        => 'Update data Satuan telah berhasil.',
+    'delete'         => 'Hapus Satuan',
+    'delete_confirm' => 'Anda yakin akan menghapus Satuan ini?',
+    'deleted'        => 'Hapus data Satuan telah berhasil.',
+    'undeleted'      => 'Data Satuan gagal dihapus.',
+    'undeleteable'   => 'Data Satuan tidak dapat dihapus.',
+
+    // Attributes
+    'name'           => 'Nama Satuan',
+];
--- resources/views/units/partials/forms.blade.php
+++ resources/views/units/partials/forms.blade.php
@@ -3,14 +3,14 @@
 @endif
 @if (Request::get('action') == 'create')
     {!! Form::open(['route' => 'units.store']) !!}
-    {!! FormField::text('name') !!}
+    {!! FormField::text('name', ['label' => trans('unit.name'), 'required' => true]) !!}
     {!! Form::submit(trans('unit.create'), ['class' => 'btn btn-success']) !!}
     {{ link_to_route('units.index', trans('app.cancel'), [], ['class' => 'btn btn-default']) }}
     {!! Form::close() !!}
 @endif
 @if (Request::get('action') == 'edit' && $editableUnit)
     {!! Form::model($editableUnit, ['route' => ['units.update', $editableUnit->id],'method' => 'patch']) !!}
-    {!! FormField::text('name') !!}
+    {!! FormField::text('name', ['label' => trans('unit.name'), 'required' => true]) !!}
     {!! Form::submit(trans('unit.update'), ['class' => 'btn btn-success']) !!}
     {{ link_to_route('units.index', trans('app.cancel'), [], ['class' => 'btn btn-default']) }}
     {!! Form::close() !!}
@@ -22,9 +22,9 @@
             <label class="control-label">{{ trans('unit.name') }}</label>
             <p>{{ $editableUnit->name }}</p>
             {!! $errors->first('unit_id', '<span class="form-error small">:message</span>') !!}
+            <hr>
+            {{ trans('unit.delete_confirm') }}
         </div>
-        <hr style="margin:0">
-        <div class="panel-body">{{ trans('app.delete_confirm') }}</div>
         <div class="panel-footer">
             {!! FormField::delete(['route'=>['units.destroy',$editableUnit->id]], trans('app.delete_confirm_button'), ['class'=>'btn btn-danger'], ['unit_id'=>$editableUnit->id]) !!}
             {{ link_to_route('units.index', trans('app.cancel'), [], ['class' => 'btn btn-default']) }}
